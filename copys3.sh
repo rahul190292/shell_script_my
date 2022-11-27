a9505d3b3a4d317b8e0133f41908c8bd9deb6482
rahul190292 <rahul190292@gmail.com>
2022-11-27 22:24:14 +0530

    copys3updatedwithpath

--- copys3.sh
+++ copys3.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 FILE_EXTENSION=$1
-for FILE_NAME in /d/AWS/AWS2021/AWSDevops/Vikas/shell_script_basics/new/forloopA/*.$FILE_EXTENSION
+for FILE_NAME in D:\AWS\AWS2021\AWSDevops\Vikas\Jenkins_setup\jinkins_integration_git\FolderA*.$FILE_EXTENSION
 do 
     echo $FILE_NAME
     aws s3 cp $FILE_NAME s3://rjcloudmagiccsv/csv/
 done
+
